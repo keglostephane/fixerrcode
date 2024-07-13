15ec9027322decd52f170d4dfda334623f3f0189
Dodzi Keglo <stephanekeglo@gmail.com>
2024-07-13 23:22:41 +0000

    Create ErrorCode scheman and model

---
+++ src/api/v1/models/errorCode.js
@@ -0,0 +1,43 @@
+import { Schema, model } from 'mongoose'
+
+const errorCodeSchema = Schema({
+  code: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  errorType: {
+    type: String,
+    enum: ['Hardware', 'Software'],
+    required: true
+  },
+  errorLevel: {
+    type: String,
+    enum: ['error', 'warning', 'info'],
+    required: true
+  },
+  reasons: {
+    type: [String],
+  },
+  solutions: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Solution',
+    required: true
+  },
+  ErrorTag: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Tag',
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, 
+{ timestamps: true }
+)
+
+export default model('ErrorCode', errorCodeSchema)
