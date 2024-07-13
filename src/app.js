35db62f817dc3a9f5bad1b1dbee17e73de277040
Dodzi Keglo <stephanekeglo@gmail.com>
2024-07-13 17:57:23 +0000

    Add AdminJs routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import dbConnect from './config/db.js'
+import { admin, adminRouter } from './admin/admin.js'
 
 const app = express()
 
+// Connect to database
 dbConnect()
+
+// AdminJS
+app.use(admin.options.rootPath, adminRouter)
 
 export default app
