dadb3235f4fcf00b2e440dd69ececc893540ee54
Dodzi Keglo <stephanekeglo@gmail.com>
2024-07-15 02:30:25 +0000

    Add api/v1 endpoints to the application

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import dbConnect from './config/db.js'
 import { admin, adminRouter } from './admin/admin.js'
+import apiRouter from './api/v1/routes/index.js'
 
 const app = express()
 
 // Connect to database
 dbConnect()
+
+app.use(express.json())
+
+// API Routes
+app.use('/api/v1', apiRouter)
 
 // AdminJS
 app.use(admin.options.rootPath, adminRouter)
